47500f51a73e4adaa120f75014f2b2d3a3caf301
Fernando <97592954+calcifersh@users.noreply.github.com>
2023-12-17 16:46:51 +0100

    Update configuracion_red.bat

--- configuracion_red.bat
+++ configuracion_red.bat
@@ -4,8 +4,8 @@ set /p choice= Quieres configurar la IP estatica? (S/N):
 if /i "%choice%"=="S" (
 
     netsh interface ip set address "Ethernet" static 192.168.1.200 255.255.255.0 192.168.1.1
-    netsh interface ip set dns "Ethernet" static 8.8.8.8
-    netsh interface ip add dns "Ethernet" 1.1.1.1 index=2
+    netsh interface ip set dns "Ethernet" static 192.168.1.50
+    netsh interface ip add dns "Ethernet" 8.8.8.8 index=2
     echo Configuracion IP estatica aplicada.
 
 ) else if /i "%choice%"=="N" (
